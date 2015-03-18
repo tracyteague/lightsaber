8b3c84493d7cf278afeac5994866e35b724f7455
Tracy Teague <tracy.teague05@gmail.com>
2015-03-18 19:07:43 -0400

    Add table to sealions migration.

--- db/migrate/20150318184857_create_sealions.rb
+++ db/migrate/20150318184857_create_sealions.rb
@@ -1,4 +1,11 @@
-        class CreateSealions < ActiveRecord::Migration
-          def change
-          end
-        end
+class CreateSealions < ActiveRecord::Migration
+  def change
+    create_table :sealions do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+      t.string :location
+      t.string :quirk
+    end
+  end
+end
