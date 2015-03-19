52c9c4e200128d503ae298523b1737bea6ec12ba
Tracy Teague <tracy.teague05@gmail.com>
2015-03-18 22:22:09 -0400

    Edit post and put tests. Provide comments and questions about how to test.

--- spec/controllers/index_spec.rb
+++ spec/controllers/index_spec.rb
@@ -2,7 +2,8 @@ require_relative '../spec_helper.rb'
 
 describe 'index.rb' do
 
-  #get
+#-----GET------
+
   describe '#get' do
 
     it 'should get the /sealions route' do
@@ -18,23 +19,43 @@ describe 'index.rb' do
 
   end
 
+#-----POST-----
 
   describe '#post' do
 
+# Would I be able to use 'let' to create a new Sealion
+# simply for testing to be available for the whole test?
+# I hard-coded an id: 5 here for testing purposes
+
     it 'should post new information to the /sealions route' do
-      post '/sealions', :params => {id: 5}
+      post '/sealions', :params => {id: 5, first_name: "Link", last_name: "Blaine", age: 27, location: "New York, NY", quirk: "Making raindrop cheek noises."}
+      expect(Sealion.find(5)).to include("Link Blaine")
+    end
+
+# Same quesiton as above re: using let
+
+    it 'should redirect to the /sealions route'
+      post '/sealions', :params => {first_name: "Link", last_name: "Blaine", age: 27, location: "New York, NY", quirk: "Making raindrop cheek noises."}
       expect(last_response).to be_redirect
       follow_redirect!
       expect(last_response).to be_ok
       expect(last_response.body).to include(Sealion.all.to_json)
     end
-
   end
 
-  #put
+#-----PUT-----
+
   describe '#put' do
 
-    it 'should return to the /sealions route' do
+# If I used let, would I be able to update the Sealion object's
+# info within the do..end block to test if it changed only
+# within the scope of this test?
+    it 'should update the params of a Sealion' do
+      put '/sealions', Sealion.find(5).update_attributes!(first_name: "Zack")
+      expect(Sealion.find(5)).to include("Zack Blaine")
+    end
+
+    it 'should redirect to the /sealions route' do
       put '/sealions', :params => with_a_param=true
       expect(last_response).to be_redirect
       follow_redirect!
@@ -44,12 +65,16 @@ describe 'index.rb' do
 
   end
 
-  #delete
-  #took out params as Ashi stated delete generally
-  #only needs the id from the route
+#-----DELETE-----
+
   describe '#delete' do
 
-    it 'should destroy information in the /sealions route' do
+    it 'should destroy a Sealion object' do
+      delete '/sealions'
+
+    end
+
+    it 'should redirect to the /sealions route' do
       delete '/sealions'
       expect(last_response).to be_redirect
       follow_redirect!
@@ -60,3 +85,5 @@ describe 'index.rb' do
   end
 
 end
+
+# NOTE TO SELF: If change redirect in post/put, fix tests
