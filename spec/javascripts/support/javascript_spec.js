26d763b0fac6994622065b6881a946314377e680
Tracy Teague <tracy.teague05@gmail.com>
2015-03-23 21:52:00 -0400

    Add working tests for Canarytest, cacalculator to exist, and functionality of functions.

---
+++ spec/javascripts/support/javascript_spec.js
@@ -0,0 +1,30 @@
+describe("Canarytest", function(){
+  it("should work", function(){
+    expect(true).toBe(true);
+  });
+});
+
+describe("calculator", function(){
+  it("should exist", function(){
+    expect(calculator).toBeDefined();
+  });
+
+  it("should have a function to find the sum of two numbers", function() {
+    expect(calculator.add(2,3)).toEqual(5);
+  });
+
+  it("should have a function to find the difference between two numbers", function(){
+    expect(calculator.subtract(5,3)).toEqual(2);
+  });
+
+  it("should have a function to find the product of two numbers", function(){
+    expect(calculator.multiply(5,3)).toEqual(15);
+  });
+
+//this one does not pass - not sure why
+  it("should have a function to find the quotient of two numbers", function(){
+    expect(calculator.divide(1,1)).toEqual(1);
+  });
+
+});
+
