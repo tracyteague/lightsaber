62a013f2371828aa86b231775f4471f37b73ced8
Tracy Teague <tracy.teague05@gmail.com>
2015-03-23 21:58:15 -0400

    Change language for tests confirming functionality.

--- spec/javascripts/support/javascript_spec.js
+++ spec/javascripts/support/javascript_spec.js
@@ -9,24 +9,24 @@ describe("calculator", function(){
     expect(calculator).toBeDefined();
   });
 
-  it("should have a function 'add()'", function() {
+  it("should have an existing function 'add()'", function() {
     expect(calculator.add).toBeDefined();
   });
 
-  it("add() should find the sum of two numbers", function() {
+  it("should have a working function add() to find the sum of two numbers", function() {
     expect(calculator.add(2,3)).toEqual(5);
   });
 
-  it("should have a function to find the difference between two numbers", function(){
+  it("should have a working function subtract() to find the difference between two numbers", function(){
     expect(calculator.subtract(5,3)).toEqual(2);
   });
 
-  it("should have a function to find the product of two numbers", function(){
+  it("should have a working function multiply() to find the product of two numbers", function(){
     expect(calculator.multiply(5,3)).toEqual(15);
   });
 
 //this one does not pass - not sure why
-  it("should have a function to find the quotient of two numbers", function(){
+  it("should have a working function divide() to find the quotient of two numbers", function(){
     expect(calculator.divide(1,1)).toEqual(1);
   });
 
