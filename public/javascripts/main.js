3e9226278ac985051f3b76d06579f9cebdf79cf3
Tracy Teague <tracy.teague05@gmail.com>
2015-03-23 21:50:11 -0400

    Add public directory.

---
+++ public/javascripts/main.js
@@ -0,0 +1,14 @@
+var calculator = {
+  add: function(x, y) {
+    return x + y
+  },
+  subtract: function(x, y) {
+    return x - y
+  },
+  multiply: function(x, y) {
+    return x * y
+  },
+  divide: function (x, y) {
+    return x / y
+  }
+}
